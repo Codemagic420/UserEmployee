9d140dd2b0b248fd13681a34fb0a1fbc84aa3b19
codemagic420 <magn554n@stud.kea.dk>
2024-09-03 13:35:20 +0200

    undskyld

--- src/main/java/org/example/useremployee/controller/EmployeeRestController.java
+++ src/main/java/org/example/useremployee/controller/EmployeeRestController.java
@@ -1,4 +1,22 @@
 package org.example.useremployee.controller;
 
+import org.example.useremployee.model.Employee;
+import org.example.useremployee.repositories.Employeerepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class EmployeeRestController {
+
+    @Autowired
+    Employeerepository employeerepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeerepository.findAll();
+    }
+
 }
